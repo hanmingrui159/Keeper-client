e5af0a33c40f3dd42a2072a79e3a890356197260
jason-ubuntu <jason@luminesence.jp>
2023-02-23 13:01:11 -0800

    update copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright Mingrui Han ⓒ {year}</p>
     </footer>
   );
 }
